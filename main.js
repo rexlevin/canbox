2a9fe4f7530e192c9456c98c3d19123242fc9b97
lizl6 <broodmantis@163.com>
2024-09-15 13:03:51 +0800

    init project

---
+++ main.js
@@ -0,0 +1,74 @@
+const { app, BrowserWindow, Menu, Tray, ipcMain, dialog, shell } = require('electron')
+const path = require('path')
+// const Store  = require('electron-store');
+const { sandboxed } = require('process');
+
+// Store.initRenderer();
+
+// 清除启动时控制台的“Electron Security Warning (Insecure Content-Security-Policy)”报错信息
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+// 禁用当前应用程序的硬件加速
+app.disableHardwareAcceleration();
+
+const isDarwin = process.platform === 'darwin' ? true : false;
+
+let win = null;
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+console.info(__dirname);
+
+const createWindow = () => {
+    // Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
+    // Menu.setApplicationMenu(null);
+
+    let config = {
+        minWidth: 820,
+        minHeight: 600,
+        icon: path.join(__dirname, '/public/logo.png'),
+        webPreferences: {
+            sandbox: false,     // 没有这个配置，加载不到 preload.js
+            preload: path.join(__dirname, '/preload.js'),
+            // preload: 'http://localhost:13000',
+            spellcheck: false
+        },
+        useContentSize: true,
+        show: false,
+        autoHideMenuBar: true,
+    };
+
+    win = new BrowserWindow(config);
+
+    // win.loadURL('file://' + __dirname + '/dist/index.html');
+    // win.loadURL('file://${__dirname}/dist/index.html');
+    win.loadURL(path.join('file://', __dirname, '/dist/index.html'));
+    
+    // win.setMenu(Menu.buildFromTemplate(menuTemplate));
+    win.setMenu(null);
+
+    // 打开开发者窗口
+    // win.webContents.openDevTools();
+
+    win.on('ready-to-show', () => {
+        win.show();
+    });
+
+    // 关闭主窗口事件，记录窗口大小和位置
+    win.on('close', (e) => {
+        // e.preventDefault();     // 阻止默认事件
+        // let isMax = win.isMaximized()
+        //     , mainPosition = win.getContentBounds();
+        // console.info('now will close app');
+        // win.webContents.send('closeApp', isMax, mainPosition);
+    });
+}   
