ac7875a9d2bdbf69ab3bf5fc890814b27c7011a8
lizl6 <broodmantis@163.com>
2025-08-08 18:55:24 +0800

    代码优化：获取app集合

--- ipcHandlers.js
+++ ipcHandlers.js
@@ -238,6 +238,10 @@ function initIpcHandlers(win) {
             throw err;
         }
     });
+
+    ipcMain.handle('getAppList', async (event) => {
+        return await getAppList();
+    });
 }
 
 async function removeAppDevById(id) {
@@ -271,7 +275,7 @@ async function getAppDevList() {
     // console.info(1,appsDevConfig);
     // console.info('getAppDevList===', appsDevConfig.get('default'));
     if(undefined === AppsDevConfig.get('default')) {
-        return null;
+    return {"correct": {}, "wrong": {}};
     }
     let /*Array<Types.AppItemType>*/ appDevInfoList = AppsDevConfig.get('default')
         , appDevList = []
@@ -299,6 +303,107 @@ async function getAppDevList() {
     // console.info('appDevInfoList===', appDevInfoList);
     // console.info('appDevList=====%o', appDevList);
     return {"correct": appDevList, "wrong": appDevFalseList};
+}
+
+/**
+ * @returns {*[Object]} app信息集合
+ */
+function getAppList() {
+    // console.log('appsConfig.get('default'):', appsConfig.get('default'));
+    if(undefined === AppsConfig.get('default')) {
+        return [];
+    }
+    const /*Array<Types.AppItemType>*/ appInfoList = AppsConfig.get('default');
+    let appList = [];
+    for(const appInfo of appInfoList) {
+        // /home/lizl6/.config/canbox/Users/apps.json
+        // C:\Users\brood\AppData\Roaming\canbox\Users\apps.json
+        // 读取app.json文件内容
+        const appJson = JSON.parse(fs.readFileSync(path.join(APP_PATH, appInfo.id + '.asar/app.json'), 'utf8'));
+        const iconPath = path.join(APP_PATH, appInfo.id + '.asar', appJson.logo);
+        console.info('iconPath: ', iconPath);
+        const app = {
+            id: appInfo.id,
+            appJson: appJson,
+            logo: iconPath,
+            path: path.join(APP_PATH, appInfo.id + '.asar')
+        };
+        appList.push(app);
+    }
+    console.info('appList=====%o', appList);
+    return appList;
+}
+
+/**
+ * 获取当前开发中的app列表
+ * @returns 获取一个json格式得app信息列表， 内容示例如下：
+{
+    "wrong": [
+        {
+            "id": "98e2dea8620745a0a49ea0dd205609da",
+            "path": "/depot/cargo/demo-app/",
+            "name": "test"
+        }
+    ],
+    "correct": [
+        {
+            "id": "691e211238c141dcb6c00de4c0416349",
+            "path": "C:\\Users\\brood\\depot\\cargo\\can-demo\\",
+            "name": "demo",
+            "appJson": {
+                "name": "demo",
+                "description": "这是一个插件demo",
+                "author": "dev001",
+                "homepage": "https://gitee.com/dev001/clipboard",
+                "main": "index.html",
+                "logo": "logo.png",
+                "version": "0.0.6",
+                "window": {
+                    "minWidth": 600,
+                    "minHeight": 400,
+                    "width": 700,
+                    "height": 500,
+                    "resizable": false
+                },
+                "platform": [ "win32", "darwin", "linux" ],
+                "categories": [ "utility" ],
+                "tags": [ "demo" ]
+            }
+        }
+    ]
+}
+ */
+
+Types = function() {}
+Types.AppItemType = function () {
+    return {
+        "name": "剪贴板",
+        "id": "com.gitee.dev001.clipboard",
+        "description": "一个好用的剪贴板",
+        "author": "dev001",
+        "homepage": "https://gitee.com/dev001/clipboard",
+        "main": "index.html",
+        "logo": "logo.png",
+        "version": "0.0.6",
+        "window": {
+            "minWidth": 800,
+            "minHeight": 400,
+            "width": 900,
+            "height": 500,
+            "icon": "logo.png",
+            "resizable": false,
+            "webPreferences": {
+                "preload": "preload.js"
+            }
+        },
+        "platform": ["win32", "darwin", "linux"],
+        "categories": ["development", "utility"],
+        "tags": ["json", "jsonformatter"],
+        "development": {
+            "main": "index.html",
+            "devTools": "detach"
+        }
+    };
 }
 
 module.exports = {
