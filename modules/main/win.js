904e041cb0b2a810f72acf8544b6c065e96d9210
lizl6 <broodmantis@163.com>
2025-08-19 23:15:13 +0800

    优化app项目新开子窗口url处理逻辑

--- modules/main/win.js
+++ modules/main/win.js
@@ -2,9 +2,11 @@ const { BrowserWindow } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
+// 初始化 pathManager 模块
+const { getAppsPath } = require('./pathManager');
+
 // 初始化 storage 实例
-const { getAppsDevStore } = require('./storageManager');
-const AppsDevConfig = getAppsDevStore();
+const { getAppsDevStore, getAppsStore } = require('./storageManager');
 
 /**
  * 窗口操作模块
@@ -42,13 +44,14 @@ const WindowManager = {
 
         try {
             // 根据 parentWindowId 判断应用类型并拼接完整路径
-            const appDevConfigArr = AppsDevConfig.get('default') || [];
+            const appDevConfigArr = getAppsDevStore().get('default') || [];
+            const appConfigArr = getAppsStore().get('default') || [];
             if (appDevConfigArr.find(item => item.id === parentWindowId)) {
-                const appDevPath = appDevConfigArr.find(item => item.id === parentWindowId).path;
-                const appJsonPath = path.join(appDevPath, 'app.json');
+                const appDevItem = appDevConfigArr.find(item => item.id === parentWindowId).path;
+                const appJsonPath = path.join(appDevItem, 'app.json');
                 const appJson = JSON.parse(fs.readFileSync(appJsonPath, 'utf-8'));
-                const uatDevJson = fs.existsSync(path.resolve(appDevPath, 'uat.dev.json'))
-                            ? JSON.parse(fs.readFileSync(path.resolve(appDevPath, 'uat.dev.json'), 'utf-8'))
+                const uatDevJson = fs.existsSync(path.resolve(appDevItem, 'uat.dev.json'))
+                            ? JSON.parse(fs.readFileSync(path.resolve(appDevItem, 'uat.dev.json'), 'utf-8'))
                             : null;
                 /*
                  * uat.dev.json中的main存在，那么以uat.dev.json中的main为基准；
@@ -56,15 +59,15 @@ const WindowManager = {
                  * uat.dev.json中的main也可能是一个相对路径，所以需要判断是否是http开头，否则需要拼接项目路径
                  */
                 const mainPath = uatDevJson
-                    ? uatDevJson.main.startsWith('http')
-                        ? uatDevJson.main
-                        : path.join(appDevPath, uatDevJson.main)
-                    : path.join(appDevPath, appJson.main);
+                    ? uatDevJson.main.startsWith('http') ? uatDevJson.main : `file://${path.join(appDevItem, uatDevJson.main)}`
+                    : appJson.main.startsWith('http') ? appJson.main : `file://${path.join(appDevItem, appJson.main)}`;
                 console.info('mainPath: ', mainPath);
                 loadURL = path.join(mainPath, loadURL);
             } else {
-                const { getAppsPath } = require('./pathManager');
-                loadURL = path.join(getAppsPath(), `${parentWindowId}.asar`, loadURL);
+                const appJson = JSON.parse(fs.readFileSync(path.join(getAppsPath(), `${parentWindowId}.asar`, 'app.json'), 'utf-8'));
+                loadURL = appJson.main.startsWith('http')
+                    ? path.join(appJson.main, loadURL)
+                    : `file://${path.join(getAppsPath(), `${parentWindowId}.asar`, appJson.main, loadURL)}`;
             }
             console.info('win.js, loadURL: ', loadURL);
 
